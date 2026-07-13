5b337016a1cce65b29680cb5a15a29e36f44ef5a
CaueCalebe <arthurcarvalhodas@gmail.com>
2026-07-13 16:47:19 -0300

    adicionando os getters and setters Pessoa.Java

--- EventoJava/src/model/Pessoa.java
+++ EventoJava/src/model/Pessoa.java
@@ -1,5 +1,28 @@
 package model;
 
 public class Pessoa {
+    String nome;
+    String email;
+    String telefone;
 
+    public String getNome() {
+        return nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
 }
