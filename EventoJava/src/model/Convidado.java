902ab4ad7a6e2fd45f7d30e1e99dbc3751f55888
Arthur-Calebe <arthurcarvalhodas@gmail.com>
2026-07-13 17:03:50 -0300

    Implementa herança em Visitante e Convidado, estendendo Pessoa com super()

--- EventoJava/src/model/Convidado.java
+++ EventoJava/src/model/Convidado.java
@@ -1,4 +1,20 @@
 package model;
 
-public class Convidado {
+public class Convidado extends Pessoa{
+    private String funcao;
+
+    
+    public Convidado(String nome, String email, String telefone, String funcao) {
+        super(nome, email, telefone);
+        this.funcao = funcao;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+    
 }
--- EventoJava/src/model/Visitante.java
+++ EventoJava/src/model/Visitante.java
@@ -1,4 +1,20 @@
 package model;
 
-public class Visitante {
+public class Visitante extends Pessoa{
+    private int idade;
+
+
+    public Visitante(String nome, String email, String telefone, int idade) {
+        super(nome, email, telefone);
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
 }
