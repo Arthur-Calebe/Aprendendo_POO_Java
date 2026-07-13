95f357437e2186e48f4f2d582e04102a0ab23ef0
Arthur-Calebe <arthurcarvalhodas@gmail.com>
2026-07-13 17:09:23 -0300

    Implementando herança em Empresas

--- EventoJava/src/model/Empresas.java
+++ EventoJava/src/model/Empresas.java
@@ -1,4 +1,22 @@
 package model;
 
 public class Empresas {
+    private String cnpj;
+    private int numStand;
+    
+
+    public Empresas(String cnpj, int numStand) {
+        this.cnpj = cnpj;
+        this.numStand = numStand;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    
 }
