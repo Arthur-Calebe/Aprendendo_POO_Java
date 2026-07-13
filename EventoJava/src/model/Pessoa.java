3486f325fb37e630ecd884bf1615f5874ef341e4
CaueCalebe <arthurcarvalhodas@gmail.com>
2026-07-13 16:59:20 -0300

    Implementa herança em Palestrante, estendendo Pessoa com super()

--- EventoJava/src/model/Pessoa.java
+++ EventoJava/src/model/Pessoa.java
@@ -1,9 +1,17 @@
 package model;
 
 public class Pessoa {
-    String nome;
-    String email;
-    String telefone;
+    private String nome;
+    private String email;
+    private String telefone;
+
+
+    public Pessoa(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
 
     public String getNome() {
         return nome;
@@ -14,7 +22,7 @@ public class Pessoa {
     public String getTelefone() {
         return telefone;
     }
-    
+
     public void setNome(String nome) {
         this.nome = nome;
     }
