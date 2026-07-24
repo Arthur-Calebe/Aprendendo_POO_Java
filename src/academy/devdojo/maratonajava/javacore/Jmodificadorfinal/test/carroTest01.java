b3327d818bc2f6f3f90001df474d915bd481a6ae
Arthur-Calebe <arthurcarvalhodas@gmail.com>
2026-07-24 20:42:09 -0300

    estudando modificador final - Tipo primitivo

---
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/carroTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.test;
+
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+
+public class carroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(carro.VELOCIDADE_LIMITE);
+
+    }
+}
