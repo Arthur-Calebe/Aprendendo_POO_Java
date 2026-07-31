1992f8d632573324d32fb8ddbf66abd5ffd275e9
Arthur-Calebe <arthurcarvalhodas@gmail.com>
2026-07-31 20:50:30 -0300

    Começando Classes Abstratas

---
+++ src/academy/devdojo/maratonajava/javacore/LclassesAbstratas/dominio/Funcionario.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.LclassesAbstratas.dominio;
+
+    // Uma classe *abstract* é uma classe que serve como modelo/base para outras classes, não pode criar objeto a partir dessa classe " new funcionario (...)"
+public abstract class Funcionario {
+    protected String nome;
+    protected double salario;
+
+    // continua tendo um construtor, pois a classe vai dar origem a outras
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/LclassesAbstratas/test/FuncionarioTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.LclassesAbstratas.test;
+
+import academy.devdojo.maratonajava.javacore.LclassesAbstratas.dominio.Desenvolvedor;
+import academy.devdojo.maratonajava.javacore.LclassesAbstratas.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.LclassesAbstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        /* Como a classe é abstrata, não pode gerar um objeto a partir dela
+        Funcionario funcionario = new Funcionario("Arthur Calebe", 2500);*/
+
+        Gerente gerente = new Gerente("Cauê Calebe", 7500, "T.I");
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Arthur Calebe", 6000);
+
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+    }
+}
